315498118b3f3114e9d0246284e0c751363df81d
yangyii0113 <yangyii0113@gmail.com>
2026-09-09 10:52:58 +0800

    feat(v12): expose policy-driven evidence pipeline

--- v12/early_trend/index.js
+++ v12/early_trend/index.js
@@ -3,4 +3,5 @@ const contracts=require('./contracts.js');
 const fusion=require('./fusion.js');
 const evidence=require('./evidence_builders.js');
 const policy=require('./evidence_policy.js');
-module.exports=Object.freeze({...contracts,...fusion,...evidence,...policy});
+const pipeline=require('./evidence_pipeline.js');
+module.exports=Object.freeze({...contracts,...fusion,...evidence,...policy,...pipeline});
