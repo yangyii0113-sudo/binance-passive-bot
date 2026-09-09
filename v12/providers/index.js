a274c14b7aa8a0539ef8939cae4a11cb74a7f8f0
yangyii0113 <yangyii0113@gmail.com>
2026-09-09 14:49:03 +0800

    feat(v12): export provider runtime governance

--- v12/providers/index.js
+++ v12/providers/index.js
@@ -3,6 +3,7 @@ const contract=require('./provider_contract.js');
 const registry=require('./registry.js');
 const catalog=require('./source_catalog.js');
 const activation=require('./activation_gate.js');
+const runtimeGovernance=require('./runtime_governance.js');
 const adapters=Object.freeze({
   twse:require('./twse_adapter.js'),
   tpex:require('./tpex_adapter.js'),
@@ -15,4 +16,4 @@ const adapters=Object.freeze({
   krx:require('./krx_adapter.js'),
   jpx:require('./jpx_adapter.js'),
 });
-module.exports=Object.freeze({...contract,...registry,...catalog,...activation,adapters});
+module.exports=Object.freeze({...contract,...registry,...catalog,...activation,...runtimeGovernance,adapters});
