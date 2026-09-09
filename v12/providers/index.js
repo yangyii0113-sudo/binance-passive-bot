48c551373e1158aab68619f86c54db59f5867cc9
yangyii0113 <yangyii0113@gmail.com>
2026-09-09 09:48:58 +0800

    feat(v12): expose BLS official adapter

--- v12/providers/index.js
+++ v12/providers/index.js
@@ -6,5 +6,6 @@ const adapters=Object.freeze({
   twse:require('./twse_adapter.js'),
   tpex:require('./tpex_adapter.js'),
   sec:require('./sec_edgar_adapter.js'),
+  bls:require('./bls_adapter.js'),
 });
 module.exports=Object.freeze({...contract,...registry,...catalog,adapters});
