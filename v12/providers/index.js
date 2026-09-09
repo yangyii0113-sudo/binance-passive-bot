5533f31bb30a0a40550adb260e7c3f0506efeed3
yangyii0113 <yangyii0113@gmail.com>
2026-09-09 09:43:15 +0800

    feat(v12): expose SEC official adapter

--- v12/providers/index.js
+++ v12/providers/index.js
@@ -2,5 +2,9 @@
 const contract=require('./provider_contract.js');
 const registry=require('./registry.js');
 const catalog=require('./source_catalog.js');
-const adapters=Object.freeze({twse:require('./twse_adapter.js'),tpex:require('./tpex_adapter.js')});
+const adapters=Object.freeze({
+  twse:require('./twse_adapter.js'),
+  tpex:require('./tpex_adapter.js'),
+  sec:require('./sec_edgar_adapter.js'),
+});
 module.exports=Object.freeze({...contract,...registry,...catalog,adapters});
