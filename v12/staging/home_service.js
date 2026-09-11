6a941a47f5013410cb42729afade01fa56f0b613
yangyii0113 <yangyii0113@gmail.com>
2026-09-11 13:08:07 +0800

    feat(v12): pass forward research tracker into Home publisher

--- v12/staging/home_service.js
+++ v12/staging/home_service.js
@@ -3,9 +3,9 @@
 const {createHomeSnapshotStore,createReadOnlyHandler}=require('./read_api.js');
 const {createHomeSnapshotPublisher}=require('./home_publisher.js');
 
-function createStagingHomeService({lineageStore}={}){
+function createStagingHomeService({lineageStore,forwardResearchTracker}={}){
   const homeStore=createHomeSnapshotStore();
-  const publisher=createHomeSnapshotPublisher({homeStore});
+  const publisher=createHomeSnapshotPublisher({homeStore,forwardResearchTracker});
   const handler=createReadOnlyHandler({homeStore,lineageStore});
 
   function publishHome(input={}){
