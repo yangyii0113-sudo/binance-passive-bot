1e1a2657dfa2cc67ac8107008ec10793a0359cde
yangyii0113 <yangyii0113@gmail.com>
2026-09-17 16:04:53 +0800

    refactor: add FOXYYA Lite runtime config

---
+++ foxyya-lite/src/config.js
@@ -0,0 +1,10 @@
+export const MARKET_SYMBOLS = Object.freeze([
+  { symbol: 'BTCUSDT', icon: '₿', display: 'BTC / USDT' },
+  { symbol: 'ETHUSDT', icon: '◆', display: 'ETH / USDT' },
+  { symbol: 'SOLUSDT', icon: 'S', display: 'SOL / USDT' }
+]);
+
+export const MARKET_CACHE_KEY = 'foxyya.market.v1';
+export const MARKET_REFRESH_MS = 30_000;
+export const MARKET_TIMEOUT_MS = 7_000;
+export const MARKET_SOURCE = 'BINANCE USD-M';
