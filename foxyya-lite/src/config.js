4ac7c98ef2325765523bc6068225f22cad2fee18
yangyii0113 <yangyii0113@gmail.com>
2026-09-17 16:13:59 +0800

    refactor: add strategy snapshot endpoint config

--- foxyya-lite/src/config.js
+++ foxyya-lite/src/config.js
@@ -8,3 +8,6 @@ export const MARKET_CACHE_KEY = 'foxyya.market.v1';
 export const MARKET_REFRESH_MS = 30_000;
 export const MARKET_TIMEOUT_MS = 7_000;
 export const MARKET_SOURCE = 'BINANCE USD-M';
+
+export const STRATEGY_ENDPOINT = '/api/strategy';
+export const SNAPSHOT_TIMEOUT_MS = 5_000;
