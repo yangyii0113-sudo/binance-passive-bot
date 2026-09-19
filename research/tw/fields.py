e663cc3ee2a7328babe3bb23c06edecce42f0154
yangyii0113 <yangyii0113@gmail.com>
2026-09-20 03:05:01 +0800

    feat(tw): add canonical margin and short field names

--- research/tw/fields.py
+++ research/tw/fields.py
@@ -3,3 +3,17 @@
 INSTITUTIONAL_BUY_AMOUNT = "institutional_buy_amount"
 INSTITUTIONAL_SELL_AMOUNT = "institutional_sell_amount"
 INSTITUTIONAL_NET_AMOUNT = "institutional_net_amount"
+
+MARGIN_BALANCE = "margin_balance"
+MARGIN_PREVIOUS_BALANCE = "margin_previous_balance"
+MARGIN_CHANGE = "margin_change"
+MARGIN_QUOTA = "margin_quota"
+MARGIN_USAGE_PERCENT = "margin_usage_percent"
+
+SHORT_BALANCE = "short_balance"
+SHORT_PREVIOUS_BALANCE = "short_previous_balance"
+SHORT_CHANGE = "short_change"
+SHORT_QUOTA = "short_quota"
+SHORT_USAGE_PERCENT = "short_usage_percent"
+
+MARGIN_SHORT_RATIO_PERCENT = "margin_short_ratio_percent"
